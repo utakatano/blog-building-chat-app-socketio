3a73812b30e5fe7f18a2c5eadf2a28d3b63ffe93
kei-yagi <kei.yagi@gmail.com>
2022-08-25 23:58:37 +0900

    add-socket-io-script

--- app/chat-app/server/index.js
+++ app/chat-app/server/index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+})
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`)
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected')
+  })
+})
+
 app.get('/api', (_, res) => {
   res.json({
     message: 'Hello world'
